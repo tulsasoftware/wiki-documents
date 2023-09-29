e48730255b2b9e9a8e36dfa32ea5733aa9ef9542
Corey Thompson <corey.dwayne.thompson@gmail.com>
2023-09-29 13:16:24 -0500

    revert 8fb1167c1f91080f35324ff127100bb94ca316f5

--- sidebars.js
+++ sidebars.js
@@ -2274,14 +2274,6 @@ const sidebars = {
             'Edge/reTerminal-DM/Embedded_ML/reTerminal-DM-edgeimpulse',
           ],
         },
-        {
-          type: 'category',
-          label: 'Ignition Edge',
-          items: [
-            'Edge/reTerminal-DM/Ignition_Edge/reTerminal-DM-Getting-Started-with-Ignition-Edge',
-            'Edge/reTerminal-DM/Ignition_Edge/reTerminal-DM-Ignition-Edge-Panel-Builder',
-          ],
-        },
       ],
     },
 
